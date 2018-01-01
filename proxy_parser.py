97a53f1a62914bea70735d1a1daa04a6ef4ecb71
AndK3d <regbox3000@gmail.com>
2018-01-01 21:28:32 +0200

    work
    parse good

--- proxy_parser.py
+++ proxy_parser.py
@@ -1,4 +1,25 @@
 import requests
-from lxml import html
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-site_url =  'https://www.ip-adress.com/proxy-list'
+
+site_url = 'https://www.ip-adress.com/proxy-list'
+html_doc = requests.get(site_url)
+
+soup = BeautifulSoup(html_doc.text, 'html.parser')
+#print(soup.prettify())
+
+for el in soup.find_all('a'):
+    #print (el)
+    continue
+
+table = soup.table
+#print(table.contents[0])
+
+
+table = soup.find("table")
+for row in table.findAll("tr"):
+    cells = row.findAll("td")
+    for cell in cells:
+        print(cell.text)
